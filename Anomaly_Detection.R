f2316abd3d94f3ea9c72d3f804d5375d7e1bddd9
PaoloDePiante <depiante.paolo@gmail.com>
2019-11-03 01:44:41 +0100

    Ver 2.0

--- Anomaly_Detection.R
+++ Anomaly_Detection.R
@@ -3,37 +3,27 @@ library(tidyverse)
 library(caret)
 library(ggpubr)
 library(StatMatch)  
-
-
 library(scales)
 
-#dat <- read.csv("C:/Lavoro/AnomalyDetect/merged_dataset_BearingTest_3.csv", sep=";", header = TRUE)
+# Downlod Data set from GitHub
+
 dat <- read.csv(url("https://raw.githubusercontent.com/PaoloDePiante/Anomaly_Detection/master/Dataset_for_Bearing.csv"), sep=",", header = TRUE)
-
-
 head(dat)
 set.seed(2)
 
+# Split data in "train set" for normal behaviour and "test set" for a failure 
 train_set <- dat[which((as.POSIXct(dat$TS, origin="1970-01-01", tz="UTC") >= as.POSIXct('2004-03-04 09:27:46', origin="1970-01-01", tz="UTC")) & (as.POSIXct(dat$TS, origin="1970-01-01", tz="UTC") <= as.POSIXct('2004-04-14 20:51:57', origin="1970-01-01", tz="UTC"))),]
 test_set <- dat[which(as.POSIXct(dat$TS, origin="1970-01-01", tz="UTC") >= as.POSIXct('2004-04-14 20:51:01', origin="1970-01-01", tz="UTC")),]
 
+# Looking to Training Data for all Bearings 
 par(mfrow=c(4,1))
-# esplora ggline e ggarrange
-plot(train_set$Bearing.1, type="l", col="dark red")
-plot(train_set$Bearing.2, type="l", col="blue")
-plot(train_set$Bearing.3, type="l", col="green")
-plot(train_set$Bearing.4, type="l", col="purple")
+plot(train_set$Bearing.1, type="l", col="dark red", main="Training Data from Bearing 1", ylab="Accelerometer Values ", xlab = "Number of data points")
+plot(train_set$Bearing.2, type="l", col="blue", main="Training Data from Bearing 2", ylab="Accelerometer Values ", xlab = "Number of data points")
+plot(train_set$Bearing.3, type="l", col="green", main="Training Data from Bearing 3", ylab="Accelerometer Values", xlab = "Number of data points")
+plot(train_set$Bearing.4, type="l", col="purple", main="Training Data from Bearing 4", ylab="Accelerometer Values", xlab = "Number of data points")
 
-# Z_train_set <- train_set %>% mutate(Bearing.1 = scale(train_set$Bearing.1), Bearing.2 = scale(train_set$Bearing.2), Bearing3 = scale(train_set$Bearing3), Bearing.4 = scale(train_set$Bearing.4))
-# il seguente esempio standardizza (ma non riduce a 0 e 1)
-# X_train_scaled = scale(X_train)
-# X_test_scaled = scale(X_test, center=attr(X_train_scaled, "scaled:center"), scale=attr(X_train_scaled, "scaled:scale"))
-# si potrebbe usare la normalizzazione seguente ma non esporta il min e max da applicare poi al test set
-# N_train_set <-normalize(train_set, range = c(0, 1), domain = range(x, na.rm = TRUE)
-
+# Normalize Data using Min Max Scaler to be in the range of [0,1]
 min_max_train <- c(min(train_set$Bearing.1),max(train_set$Bearing.1), min(train_set$Bearing.2),max(train_set$Bearing.2), min(train_set$Bearing.3),max(train_set$Bearing.3), min(train_set$Bearing.4),max(train_set$Bearing.4))
-
-
 
 Z_train_set <- train_set %>% mutate(Bearing.1 = ((train_set$Bearing.1-min_max_train[1])/(min_max_train[2]-min_max_train[1])),
                                     Bearing.2 = ((train_set$Bearing.2-min_max_train[3])/(min_max_train[4]-min_max_train[3])),
@@ -46,32 +36,28 @@ Z_test_set <- test_set %>% mutate(Bearing.1 = ((test_set$Bearing.1-min_max_train
                                   Bearing.4 = ((test_set$Bearing.4-min_max_train[7])/(min_max_train[8]-min_max_train[7])))
 
 
-
 par(mfrow=c(1,1))
 
-
+# Reduce to two principal components the sensor readings for training data normalized
 Train.pca <- prcomp(Z_train_set[2:5])
-#summary(Train.pca)
-
-
+# Predict principal components based on Training ones using test set normalized
 Test.pca <- predict(Train.pca, newdata = Z_test_set[2:5])
-#test.data <- as.data.frame(test.data)
+
+# Convert Matrix in Data Frame just for the first two principal components
 Test.pca <- as.data.frame(Test.pca[,1:2])
 Train.pca <- (Train.pca$x[,1:2])
 Train.pca <- as.data.frame(Train.pca[,1:2])
-#Test.pca <- prcomp(Z_test_set[2:5], center = TRUE, scale. = TRUE)
-#summary(Test.pca)
-#Test.pca <- Test.pca$x[,1:2]
 
-#a_train_set <- train_set[,-1]
-
+# Since the unit of measure is the same the matrix of covariance can be used.
 covariance_matrix <- cov(Train.pca)
 inv_covariance_matrix <- cov(Train.pca)^-1
 mean_distr <- colMeans(Train.pca)
 
+# Mahalanobis distance calculation both for Train and Test Set
 dist_train <- sqrt(mahalanobis(Train.pca, mean_distr, covariance_matrix, inverted = FALSE))
 dist_test <- sqrt(mahalanobis(Test.pca, mean_distr, covariance_matrix, inverted = FALSE))
 
+# Threshold calculation to identify outliers (anomalies) based on training data
 MD_threshold <- function(dist, extreme){
   if (extreme) {k=3}
   else {k=2}
@@ -79,29 +65,21 @@ MD_threshold <- function(dist, extreme){
   return(threshold)
 }
 
-threshold = MD_threshold(dist_train, extreme = TRUE)
 
-s <- (dist_train)^2
-hist(s, include.lowest = TRUE, right = TRUE,
-     angle = 45, col = "LightBlue", 
-     axes = TRUE, plot = TRUE, labels = FALSE,
-     ylim = range(0,80), xlim = range(0,14),
-     warn.unused = TRUE, xlab = "Square of the Mahalanobis distance", main = "Square of the Mahalanobis distance Distribution")
-
-
+# The Hist to set the threshold to say when could be identified an anomaly 
 d <- dist_train
 hist(d, include.lowest = TRUE, right = TRUE, freq = FALSE,
      angle = 45, col = "LightBlue", 
      axes = TRUE, plot = TRUE, labels = FALSE,
-     xlim = range(0,4), xlab ="Mahalanobis Distance", main = "Mahalanobis Distance Distribution")
+     xlim = range(0,5), xlab ="Mahalanobis Distance", main = "Mahalanobis Distance Distribution")
 
+# 3 sigma from mean distance has been used for upper threshold
+threshold = MD_threshold(dist_train, extreme = TRUE)
 
-
-
-# OK soglia ok 
-
+# Threshold value
 threshold
 
+# Show the entire period observed joining Trainind and Testing Data
 v_dist_train <-as.data.frame(dist_train)
 v_dist_test <-as.data.frame(dist_test)
 colnames(v_dist_train) <- "MD"
@@ -118,15 +96,22 @@ Train_Test <- log(Train_Test)
 
 Anomaly <- as.data.frame(cbind(Ind_Train_Test,Train_Test))
 
-write.table(Anomaly, file="C:/Lavoro/AnomalyDetect/paolo.csv", quote=T, sep=";", dec=".", na="NA", row.names=T, col.names=T)
-
-plot(Anomaly)
-title(main="Gear Vibration", col.main="blue", font.main="4")
-lines(Anomaly, col = "Blue")
-#abline(h=log(threshold))
-abline(h=log(threshold), col = "Red")
-
 df <- Anomaly %>% mutate(Time = as.POSIXct(Anomaly$Time, origin="1970-01-01", tz="UTC"))
 p <- ggplot(df,aes(x=df$Time, y=df$MD))
 p$labels$y <- "Gear Vibration"
-p + scale_x_datetime(breaks = date_breaks("1 day"), name = "Time Monitoring") + geom_line(col = "Blue") + geom_hline(yintercept=log(threshold), col = "Red")
+p + scale_x_datetime(breaks = date_breaks("1 week"), name = "Time Monitoring") + 
+  geom_line(col = "Blue") + 
+  geom_hline(yintercept=log(threshold), col = "Red") +
+  ggtitle("Engine failure long after crossing the red line")
+
+# A zoom on the failure point
+
+df <- Anomaly[5500:6324,] 
+df <- df %>% mutate(Time = as.POSIXct(df$Time, origin="1970-01-01", tz="UTC"))
+
+p <- ggplot(df,aes(x=df$Time, y=df$MD))
+p$labels$y <- "Gear Vibration"
+p + scale_x_datetime(breaks = date_breaks("1 day"), name = "Time Monitoring") + 
+  geom_line(col = "Blue") + 
+  geom_hline(yintercept=log(threshold), col = "Red") +
+  ggtitle("Zoom on Engine failure")
